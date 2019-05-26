ec077e3505f46ef781df064b5900952a04b849d1
Naveen Lingaraj <Naveen.Lingaraj@ey.com>
2019-05-27 00:08:21 +0530

    few updates

---
+++ src/blocks/top-panel/controllers/top-panel.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+ 
+})
+export default class TopPanel extends Vue {
+}
+</script>
+
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+ <template>
+    <div>Test
+      <TopPanel/>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import 'bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
+import TopPanel from '../blocks/top-panel/views/top-panel.vue';
+
+@Component({
+  components: {
+    TopPanel,
+  },
+})
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
