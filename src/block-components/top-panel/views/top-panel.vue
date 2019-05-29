53e790a091370c04d38e465c6553a8fc521e710d
Naveen Lingaraj <Naveen.Lingaraj@ey.com>
2019-05-30 00:08:43 +0530

    few updates

--- src/block-components/top-panel/views/top-panel.vue
+++ src/block-components/top-panel/views/top-panel.vue
@@ -1,7 +1,7 @@
  <template>
    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#"><span class="avatar">NL</span></a>
+                    <a class="navbar-brand" href="#"><span class="avatar">TS</span></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
